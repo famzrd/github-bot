2703c0209fa979e203c2239620004a4ebf80b6ff
Prateek Rastogi <prastogi34@gmail.com>
2020-02-19 00:19:28 +0530

    added webpack watch and configured run-all parrallel

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,28 @@
 const path = require('path')
 
- module.exports ={
-    "mode": "none",
-    "entry": {
-        drivers: './drivers/background.js',
-        views: './views/context.js',
-    },
-    "output": {
-        "path": path.resolve(__dirname),
-        "filename": "[name]/[name].bundle.js"
-    },
-    "module": {
-        rules: [
-            {
-              test: /\.m?js$/,
-              exclude: /(node_modules|bower_components)/,
-              use: {
-                loader: 'babel-loader',
-                options: {
-                  presets: ['@babel/preset-env']
-                }
-              }
-            }
-          ]
-    }
+module.exports = {
+  mode: 'none',
+  watch: true,
+  entry: {
+    drivers: './drivers/background.js',
+    views: './views/context.js'
+  },
+  output: {
+    path: path.resolve(__dirname),
+    filename: '[name]/[name].bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
 }
