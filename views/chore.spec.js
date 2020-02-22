867db8fdde218713df388d72b0c26b96c32fdeb3
Prateek Rastogi <prastogi34@gmail.com>
2020-02-23 01:28:40 +0530

    added baseline specs

--- views/chore.spec.js
+++ views/chore.spec.js
@@ -1,54 +1,17 @@
-function eventHandler (event) {
-  if (event.target.dataset.action === 'create') {
-    browser.tabs.create({
-      url: 'about:blank',
-      cookieStoreId: event.target.dataset.identity
+context('chore', () => {
+  beforeEach(() => {
+    cy.visit('https://github.com')
+  })
+
+    it('Commits Access Token', () =>{
+       
     })
-  }
-  if (event.target.dataset.action === 'close-all') {
-    browser.tabs.query({
-      cookieStoreId: event.target.dataset.identity
-    }).then((tabs) => {
-      browser.tabs.remove(tabs.map((i) => i.id))
+
+    it('Fulfils Birth Obligations', () =>{
+       
     })
-  }
-  event.preventDefault()
-}
 
-function createOptions (node, identity) {
-  for (const option of ['Create', 'Close All']) {
-    const a = document.createElement('a')
-    a.href = '#'
-    a.innerText = option
-    a.dataset.action = option.toLowerCase().replace(' ', '-')
-    a.dataset.identity = identity.cookieStoreId
-    a.addEventListener('click', eventHandler)
-    node.appendChild(a)
-  }
-}
+    it('Chats For Support', () => {
 
-var div = document.getElementById('identity-list')
-
-if (browser.contextualIdentities === undefined) {
-  div.innerText = 'browser.contextualIdentities not available. Check that the privacy.userContext.enabled pref is set to true, and reload the add-on.'
-} else {
-  browser.contextualIdentities.query({})
-    .then((identities) => {
-      if (!identities.length) {
-        div.innerText = 'No identities returned from the API.'
-        return
-      }
-
-      for (const identity of identities) {
-        const row = document.createElement('div')
-        const span = document.createElement('span')
-        span.className = 'identity'
-        span.innerText = identity.name
-        span.style = `color: ${identity.color}`
-        console.log(identity)
-        row.appendChild(span)
-        createOptions(row, identity)
-        div.appendChild(row)
-      }
     })
-}
+})
---
+++ views/signup.spec.js
@@ -0,0 +1,10 @@
+context('signup', () => {
+  beforeEach(() => {
+    cy.visit('https://github.com')
+  })
+
+    it('Fills Github Signup Form', () =>{
+      cy.wait()
+    })
+    
+})
